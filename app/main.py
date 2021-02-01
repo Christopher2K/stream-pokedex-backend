cbb7fa155c8295ebb627ab8db91a70da8a34f683
Christopher N. KATOYI <me@christopher2k.dev>
2021-02-01 19:00:20 +0100

    :sparkles: CORS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from tortoise.contrib.fastapi import register_tortoise
 
 from app import settings
 from app.api import pokemon_router
 
 root = FastAPI()
+
+origins = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
+
+root.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @root.get("/")
